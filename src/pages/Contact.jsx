f2e35e8f8b50e61c9658da39abeee4dc08ff34b7
Mahmud Sairoon <smbdmahmud1010@gmail.com>
2024-07-24 23:14:50 +0600

    contact added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <title>Notify | Contact</title>
       </Helmet>
       <div className="container w-full h-screen  flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-center text-gray-500">Contact is Unavailable</h1>
+      <h1 className="text-3xl font-bold text-center text-gray-500">For Contact me, please click <a href="https://sairoon.com/" target="_blank" className="text-blue-500 underline">Here</a></h1>
 
       </div>
 
